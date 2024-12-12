3e4d55409da57e57d69225f9bdac283b5c37b95d
Alihan Girgin <alihangir@gmail.com>
2024-12-12 15:46:20 +0300

    categorycontroller test, product controller test added

---
+++ 1likteEcommerce.Api/UnitTests/CategoryControllerTest.cs
@@ -0,0 +1,122 @@
+﻿using _1likteEcommerce.Api.Controllers;
+using _1likteEcommerce.Core.Constants;
+using _1likteEcommerce.Core.Dtos;
+using _1likteEcommerce.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace _1likteEcommerce.Api.UnitTests
+{
+    public class CategoryControllerTests
+    {
+        private readonly Mock<ICategoryService> _categoryServiceMock;
+        private readonly CategoryController _controller;
+
+        public CategoryControllerTests()
+        {
+            _categoryServiceMock = new Mock<ICategoryService>();
+            _controller = new CategoryController(_categoryServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task CreateCategory_ReturnsOk()
+        {
+            // Arrange
+            var model = new CategoryCreateDto { Title = TestUserConstants.UnitTestTitle, Description = TestUserConstants.UnitTestDescription };
+            _categoryServiceMock.Setup(x => x.AddCategoryAsync(model))
+                                .ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.CreateCategory(model);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("category created", okResult.Value);
+        }
+
+        [Fact]
+        public async Task CreateCategory_ReturnsFailed()
+        {
+            // Arrange
+            var model = new CategoryCreateDto { Title = TestUserConstants.UnitTestTitle };
+            _categoryServiceMock.Setup(x => x.AddCategoryAsync(model))
+                                .ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.CreateCategory(model);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("category cannot created", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task UpdateCategory_ReturnsOk()
+        {
+            // Arrange
+            var model = new CategoryCreateDto { Title = TestUserConstants.UnitTestTitle, Description = TestUserConstants.UnitTestDescription };
+            _categoryServiceMock.Setup(x => x.UpdateCategoryAsync(TestUserConstants.UnitTestCategoryId, model))
+                                .ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.UpdateCategory(TestUserConstants.UnitTestCategoryId, model);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("category updated", okResult.Value);
+        }
+
+        [Fact]
+        public async Task UpdateCategory_ReturnsFailed()
+        {
+            // Arrange
+            var model = new CategoryCreateDto { Title = TestUserConstants.UnitTestTitle, Description = TestUserConstants.UnitTestDescription };
+            _categoryServiceMock.Setup(x => x.UpdateCategoryAsync(TestUserConstants.UnitTestCategoryId, model))
+                                .ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.UpdateCategory(999999, model);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("category cannot updated", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task GetCategory_ReturnsOk()
+        {
+            // Arrange
+            var category = new CategoryDto { Id = TestUserConstants.UnitTestCategoryId, Title = TestUserConstants.UnitTestTitle, Description = TestUserConstants.UnitTestDescription };
+            _categoryServiceMock.Setup(x => x.GetCategoryAsync(TestUserConstants.UnitTestCategoryId))
+                                .ReturnsAsync(category);
+
+            // Act
+            var result = await _controller.GetCategory(TestUserConstants.UnitTestCategoryId);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(category, okResult.Value);
+        }
+
+
+        [Fact]
+        public async Task GetCategories_ReturnsOk()
+        {
+            // Arrange
+            var categories = new List<CategoryDto>
+            {
+                new CategoryDto { Id = TestUserConstants.UnitTestCategoryId, Title = TestUserConstants.UnitTestTitle, Description = TestUserConstants.UnitTestDescription }
+            };
+            _categoryServiceMock.Setup(x => x.GetCategoriesAsync())
+                                .ReturnsAsync(categories);
+
+            // Act
+            var result = await _controller.GetCategories();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(categories, okResult.Value);
+        }
+    }
+}
---
+++ 1likteEcommerce.Api/UnitTests/ProductControllerTest.cs
@@ -0,0 +1,160 @@
+﻿using _1likteEcommerce.Api.Controllers;
+using _1likteEcommerce.Core.Constants;
+using _1likteEcommerce.Core.Dtos;
+using _1likteEcommerce.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace _1likteEcommerce.Api.UnitTests
+{
+    public class ProductControllerTests
+    {
+        private readonly Mock<IProductService> _productServiceMock;
+        private readonly ProductController _controller;
+
+        public ProductControllerTests()
+        {
+            _productServiceMock = new Mock<IProductService>();
+            _controller = new ProductController(_productServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task CreateProduct_ReturnsOk()
+        {
+            // Arrange
+            var model = new ProductCreateDto
+            {
+                Title = TestUserConstants.UnitTestTitle,
+                Description = TestUserConstants.UnitTestDescription,
+                Price = 100,
+                CategoryId = TestUserConstants.UnitTestCategoryId
+            };
+            _productServiceMock.Setup(x => x.AddProductAsync(model))
+                               .ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.CreateProduct(model);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("product created", okResult.Value);
+        }
+
+        [Fact]
+        public async Task CreateProduct_ReturnsFailed()
+        {
+            // Arrange
+            var model = new ProductCreateDto
+            {
+                Title = TestUserConstants.UnitTestTitle,
+                Description = TestUserConstants.UnitTestDescription,
+                Price = 100,
+                CategoryId = 999999
+            };
+            _productServiceMock.Setup(x => x.AddProductAsync(model))
+                               .ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.CreateProduct(model);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("product cannot created", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task UpdateProduct_ReturnsOk()
+        {
+            // Arrange
+            var model = new ProductCreateDto
+            {
+                Title = TestUserConstants.UnitTestTitle,
+                Description = TestUserConstants.UnitTestDescription,
+                Price = 100,
+                CategoryId = TestUserConstants.UnitTestCategoryId
+            };
+            _productServiceMock.Setup(x => x.UpdateProductAsync(TestUserConstants.UnitTestProductId, model))
+                               .ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.UpdateProduct(TestUserConstants.UnitTestProductId, model);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("product updated", okResult.Value);
+        }
+
+        [Fact]
+        public async Task UpdateProduct_ReturnsFailed()
+        {
+            // Arrange
+            var model = new ProductCreateDto
+            {
+                Title = TestUserConstants.UnitTestTitle,
+                Description = TestUserConstants.UnitTestDescription,
+                Price = 100,
+                CategoryId = 999999
+            };
+            _productServiceMock.Setup(x => x.UpdateProductAsync(TestUserConstants.UnitTestProductId, model))
+                               .ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.UpdateProduct(999999, model);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("product cannot updated", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task GetProduct_ReturnsOk()
+        {
+            // Arrange
+            var product = new ProductDto
+            {
+                Id = TestUserConstants.UnitTestProductId,
+                Title = TestUserConstants.UnitTestTitle,
+                Description = TestUserConstants.UnitTestDescription,
+                Price = 100,
+                CategoryId = TestUserConstants.UnitTestCategoryId
+            };
+            _productServiceMock.Setup(x => x.GetProductAsync(TestUserConstants.UnitTestProductId))
+                               .ReturnsAsync(product);
+
+            // Act
+            var result = await _controller.GetProduct(TestUserConstants.UnitTestProductId);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(product, okResult.Value);
+        }
+
+
+        [Fact]
+        public async Task GetProducts_ReturnsOk()
+        {
+            // Arrange
+            var products = new List<ProductDto>
+            {
+                new ProductDto
+                {
+                    Id = TestUserConstants.UnitTestProductId,
+                    Title = TestUserConstants.UnitTestTitle,
+                    Description = TestUserConstants.UnitTestDescription,
+                    Price = 100.00m,
+                    CategoryId = TestUserConstants.UnitTestCategoryId
+                }
+            };
+            _productServiceMock.Setup(x => x.GetProductsAsync())
+                               .ReturnsAsync(products);
+
+            // Act
+            var result = await _controller.GetProducts();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(products, okResult.Value);
+        }
+    }
+}
